1edaad60bb28d8432d1d846d60761435abab8f22
Andy Clifton <andy_clifton@yahoo.com>
2022-11-24 15:45:12 +0100

    UDPATE:GUI

--- pages/home.py
+++ pages/home.py
@@ -10,11 +10,54 @@ import dash_bootstrap_components as dbc
 # Register this page
 # --------------------
 
-dash.register_page(__name__, path = "/")
+dash.register_page(__name__, path="/")
 
 layout = dbc.Container(
-[
-    html.H1("Home page")
-
-]
+    [
+        # title row
+        dbc.Row(
+            [
+                dbc.Col([html.H1("Wind Energy Apps")], width=12),
+            ],
+            className="title h-10 pt-2 mb-2",
+        ),
+        # content row
+        dbc.Row(
+            dbc.Col(
+                [
+                    dbc.Card(
+                        [
+                            dbc.CardImg(
+                                src="/assets/images/explorer.png", top=True
+                            ),
+                            dbc.CardBody(
+                                [
+                                    html.H4(
+                                        "Wind Energy R&D Facilities",
+                                        className="card-title",
+                                    ),
+                                    html.P(
+                                        "A searchable overview of wind energy R&D facilities",
+                                        className="card-text",
+                                    ),
+                                    html.Div(
+                                        [
+                                            dbc.Button(
+                                                "Go", color="primary", href="explorer"
+                                            ),
+                                        ],
+                                        className="d-grid gap-2 col-6 mx-auto",
+                                    ),
+                                ]
+                            ),
+                        ],
+                        style={"width": "18rem"},
+                    )
+                ]
+            )
+        ),
+    ],
+    fluid=True,
+    className="dbc h-80",
+    style={"min-height": "80vh"},
 )
